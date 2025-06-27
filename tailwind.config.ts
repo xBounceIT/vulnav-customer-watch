9f0619615ad96f6a29b79ea37dd06d213c3c1445
Daniel D'Angeli <35002896+xBounceIT@users.noreply.github.com>
2025-06-27 22:49:17 +0200

    fix workflow lint issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
 
 export default {
 	darkMode: ["class"],
@@ -92,5 +93,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+        plugins: [animatePlugin],
 } satisfies Config;
